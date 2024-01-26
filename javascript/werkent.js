c345fffed7424bfc7ec56c66a2f7aaac6c14b691
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2024-01-26 09:36:31 +0100

    taal_knop

--- javascript/werkent.js
+++ javascript/werkent.js
@@ -2,3 +2,11 @@ function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: 'smooth' });
 }
+
+const selector = document.getElementById("language-selector");
+
+for (const child of selector.children) {
+    if (child.href.endsWith(window.location.pathname)) {
+        child.classList = "selected";
+    }
+}
