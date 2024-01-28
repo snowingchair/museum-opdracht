c5485f65bc664fb921408ecdae67ebd83afb8fa7
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2024-01-28 15:01:02 +0100

    sticky_header

--- javascript/werkent.js
+++ javascript/werkent.js
@@ -1,8 +1,3 @@
-function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    section.scrollIntoView({ behavior: 'smooth' });
-}
-
 const selector = document.getElementById("language-selector");
 
 for (const child of selector.children) {
